5e776dcb33a2c049eef081df8c16e92ae73447be
Soham Kanji <sohamkanji02@gmail.com>
2025-03-09 21:15:16 +0530

    Added translation logic for triangle

---
+++ app/main.cpp
@@ -0,0 +1,214 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <iostream>
+
+// Window Dimensions
+const GLint WIDTH = 800;
+const GLint HEIGHT = 600;
+
+GLuint vao, vbo, shader, uniform_model;
+
+float x_offset = 0.0f;
+float delta = 0.005f;
+float max_offset = 0.7f;
+bool direction = true;
+
+static const char* VERTEX_SHADER = "            \n\
+#version 330                                    \n\
+                                                \n\
+layout(location = 0) in vec3 pos;               \n\
+                                                \n\
+uniform mat4 model;                            \n\
+void main()                                     \n\
+{                                               \n\
+    gl_Position = model * vec4(pos.x, pos.y, pos.z, 1.0);               \n\
+}                                               \n\
+";
+
+static const char* FRAGMENT_SHADER = "          \n\
+#version 330                                    \n\
+                                                \n\
+out vec4 color;                                 \n\
+                                                \n\
+void main()                                     \n\
+{                                               \n\
+    color = vec4(1.0, 0.0, 0.0, 1.0);           \n\
+}                                               \n\
+";
+
+void createTriangle()
+{
+    GLfloat vertices[] = {
+        -1.0f, -1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f,
+        0.0f, 1.0f, 0.0f
+    };
+
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+
+void addShader(GLuint shader_program, const char* shader_code, GLenum shader_type)
+{
+    auto the_shader = glCreateShader(shader_type);
+
+    const GLchar* the_code[1];
+    the_code[0] = shader_code;
+
+    GLint code_length[1];
+    code_length[0] = strlen(shader_code);
+
+    glShaderSource(the_shader, 1, the_code, code_length);
+    glCompileShader(the_shader);
+
+    GLint result = 0;
+    GLchar eLog[1024] = {0};
+    glGetShaderiv(the_shader, GL_COMPILE_STATUS, &result);
+    if(!result) {
+        glGetShaderInfoLog(the_shader, sizeof(eLog), nullptr, eLog);
+        std::cerr << "Error compiling the " << shader_type << " shader: " << eLog << std::endl;
+        return;
+    }
+
+    glAttachShader(shader_program, the_shader);
+}
+
+void compileShader()
+{
+    shader = glCreateProgram();
+
+    if(!shader) {
+        std::cerr << "Error creating shader program" << std::endl;
+        return;
+    }
+
+    addShader(shader, VERTEX_SHADER, GL_VERTEX_SHADER);
+    addShader(shader, FRAGMENT_SHADER, GL_FRAGMENT_SHADER);
+
+    GLint result = 0;
+    GLchar eLog[1024] = {0};
+
+    glLinkProgram(shader);
+    glGetProgramiv(shader, GL_LINK_STATUS, &result);
+
+    if(!result) {
+        glGetProgramInfoLog(shader, sizeof(eLog), nullptr, eLog);
+        std::cerr << "Error linking program: " << eLog << std::endl;
+        return;
+    }
+
+    glBindVertexArray(vao);
+    glValidateProgram(shader);
+    glGetProgramiv(shader, GL_VALIDATE_STATUS, &result);
+    if(!result) {
+        glGetProgramInfoLog(shader, sizeof(eLog), nullptr, eLog);
+        std::cerr << "Error validating program: " << eLog << std::endl;
+        return;
+    }
+    
+    uniform_model = glGetUniformLocation(shader, "model");
+
+    glBindVertexArray(0);
+}
+
+
+int main() 
+{
+    // Initialize GLFW
+    if(!glfwInit())
+    {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+
+    // Setup GLFW window properties
+    // OpenGL version
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // Core profile = No Backwards Compatibility
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // Allow Forward Compatibility
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    auto main_window = glfwCreateWindow(WIDTH, HEIGHT, "Test Window", nullptr, nullptr);
+    if(main_window == nullptr) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+
+    // Get Buffer size information
+    int buffer_width, buffer_height;
+    glfwGetFramebufferSize(main_window, &buffer_width, &buffer_height);
+
+    // Set context for GLEW to use
+    glfwMakeContextCurrent(main_window);
+
+    // Allow modern extension features
+    glewExperimental = GL_TRUE;
+
+    if(glewInit() != GLEW_OK) 
+    {
+        std::cerr << "Failed to initialize GLEW" << std::endl;
+        glfwDestroyWindow(main_window);
+        glfwTerminate();
+        return 1;
+    }
+
+    // Setup Viewport size
+    glViewport(0, 0, buffer_width, buffer_height);
+
+    createTriangle();
+    compileShader();
+    
+    // Loop until window closed
+    while(!glfwWindowShouldClose(main_window))
+    {
+        // Get + Handle user input events
+        glfwPollEvents();
+
+        if(direction) {
+            x_offset += delta;
+        } else {
+            x_offset -= delta;
+        }
+
+        if(abs(x_offset) >= max_offset) {
+            direction = !direction;
+        }
+
+        // Clear window
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shader);
+
+        glm::mat4 model(1.0f);
+        model = glm::translate(model, glm::vec3(x_offset, 0.0f, 0.0f));
+        glUniformMatrix4fv(uniform_model, 1, GL_FALSE, glm::value_ptr(model));
+
+        glBindVertexArray(vao);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+
+        // Swap the front and back buffers
+        glfwSwapBuffers(main_window);
+    }
+}
